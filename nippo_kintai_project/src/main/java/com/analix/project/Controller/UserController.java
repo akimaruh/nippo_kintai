b506adee0c1fc63818cbb24fb5c23563bf097083
a-akimaru <akimaruh@analix.co.jp>
2024-07-11 17:57:44 +0900

    fix:秋丸　UserController.java　ユーザー管理画面作成 add:秋丸　regist.html,UserService.java ユーザー管理画面作成

--- nippo_kintai_project/src/main/java/com/analix/project/Controller/UserController.java
+++ nippo_kintai_project/src/main/java/com/analix/project/Controller/UserController.java
@@ -1,5 +1,19 @@
 package com.analix.project.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class UserController {
+	
+	
+	
+	@RequestMapping(path = "/user/regist")
+	public String showUserRegist() {
+		
+		return "user/regist";
+	}
+	
+	
 
 }
