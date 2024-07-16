2ac0c618701e52a84ffd7039070ac07e22b1fe04
m-tsurumi <tsurumim@analix.co.jp>
2024-07-16 11:31:00 +0900

    add:鶴見　entity作成、コントローラのパッケージ名をController→controllerに修正

---
+++ nippo_kintai_project/src/main/java/com/analix/project/entity/Users.java
@@ -0,0 +1,62 @@
+package com.analix.project.entity;
+
+import java.sql.Date;
+
+public class Users {
+
+	private int id;
+	private String password;
+	private String name;
+	private String role;
+	private int departmentId;
+	private Date startDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public int getDepartmentId() {
+		return departmentId;
+	}
+
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+}
