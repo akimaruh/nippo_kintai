03fe02cdb05d142c4e1cf8c50e71526ad548a588
a-akimaru <akimaruh@analix.co.jp>
2024-07-16 15:53:26 +0900

    fix:秋丸

--- nippo_kintai_project/src/main/java/com/analix/project/controller/UserController.java
+++ nippo_kintai_project/src/main/java/com/analix/project/controller/UserController.java
@@ -4,16 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 	
 	
 	
-	@RequestMapping(path = "/user/regist")
+	@RequestMapping(path = "/regist")
 	public String showUserRegist() {
 		
 		return "user/regist";
 	}
 	
+	@RequestMapping(path = "/regist/search")
+	public String searchUserByUserID() {
+		return "user/regist";
+	}
+	
+	@RequestMapping(path = "/regist/complete")
+	public String copleteUserRegist() {
+		return "user/regist";
+	}
 	
 
 }
--- nippo_kintai_project/src/main/java/com/analix/project/mapper/UserMapper.java
+++
@@ -1,5 +0,0 @@
-package com.analix.project.mapper;
-
-public interface UserMapper {
-
-}
