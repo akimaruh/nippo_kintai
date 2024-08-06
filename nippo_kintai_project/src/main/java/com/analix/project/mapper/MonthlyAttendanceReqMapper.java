15480435c5478fe4689039ca305ffc9a1bf72dc2
m-tsurumi <tsurumim@analix.co.jp>
2024-08-06 09:37:15 +0900

    fix:鶴見

--- nippo_kintai_project/src/main/java/com/analix/project/mapper/MonthlyAttendanceReqMapper.java
+++ nippo_kintai_project/src/main/java/com/analix/project/mapper/MonthlyAttendanceReqMapper.java
@@ -1,5 +1,6 @@
 package com.analix.project.mapper;
 
+import java.sql.Date;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
@@ -26,6 +27,16 @@ public interface MonthlyAttendanceReqMapper {
 	public void insertMonthlyAttendanceReq(@Param("monthlyDto") MonthlyAttendanceReqDto monthlyDto);
 	
 	/*
+	 * 「承認申請」ボタン押下後 - status検索
+	 */
+	public Integer findStatusByUserIdAndYearMonth(@Param("userId") Integer userId, @Param("attendanceDate") Date attendanceDate);
+
+	/*
+	 * status更新 承認申請
+	 */
+	public void updateStatusWaiting(@Param("userId") Integer userId, @Param("attendanceDate") Date attendanceDate);
+	
+	/*
 	 * status更新 承認
 	 */
 	public void updateStatusApprove(@Param("userId") Integer userId, @Param("targetYearMonth") String targetYearMonth);
@@ -34,5 +45,10 @@ public interface MonthlyAttendanceReqMapper {
 	 * status更新 却下
 	 */
 	public void updateStatusReject(@Param("userId") Integer userId, @Param("targetYearMonth") String targetYearMonth);
+	
+	/*
+	 * ユーザー名取得
+	 */
+	public MonthlyAttendanceReqDto findMonthlyAttendanceReqByUserId(@Param("userId") Integer userId, @Param("targetYearMonth") String targetYearMonth);
 
 }
