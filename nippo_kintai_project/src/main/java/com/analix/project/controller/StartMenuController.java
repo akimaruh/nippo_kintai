97c7d4b194cdd4c14e3945d2af0b51ae844b2710
a-akimaru <akimaruh@analix.co.jp>
2024-08-21 10:48:08 +0900

    fix:秋丸　『ログオフ』ボタン押下時のセッション破棄とリダイレクト時にGET　notSupportedのエラーがでないよう、クラス定義時の@RequestMapping(/common)を廃止それに伴う修正済み

--- nippo_kintai_project/src/main/java/com/analix/project/controller/StartMenuController.java
+++ nippo_kintai_project/src/main/java/com/analix/project/controller/StartMenuController.java
@@ -2,18 +2,20 @@ package com.analix.project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping("/common")
 public class StartMenuController {
 
 	/**
 	 * 初期表示
 	 * @return
 	 */
-	@RequestMapping(path = "/startMenu")
+	@RequestMapping(path = "/common/startMenu", method = RequestMethod.POST)
 	public String showMenu(HttpSession session) {
 
 		return "common/startMenu";
@@ -23,9 +25,14 @@ public class StartMenuController {
 	 * 『ログオフ』ボタン押下後
 	 * @return ページを閉じる
 	 */
-	@RequestMapping(path = "/logoff")
-	public String logoff(HttpSession session) {
-		session.invalidate();
-		return "redirect:/login";//JavaScriptで閉じる
+	@RequestMapping(path = "/common/logoff", method = RequestMethod.POST)
+	public String logoff(HttpServletRequest request, HttpServletResponse response) {
+
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			session.invalidate();
+		}
+
+		return "redirect:/";//JavaScriptで閉じないならこっち
 	}
 }
