0bea56da9c4a1966687f683c77483bacb1e23dc6
a-akimaru <akimaruh@analix.co.jp>
2024-08-21 10:57:29 +0900

    add:秋丸　部署管理画面用ファイル作成

---
+++ nippo_kintai_project/src/main/java/com/analix/project/entity/Department.java
@@ -0,0 +1,12 @@
+package com.analix.project.entity;
+
+import lombok.Data;
+
+@Data
+public class Department {
+
+	private Integer departmentId;
+	private String departmentName;
+	private Byte isActive;
+
+}
