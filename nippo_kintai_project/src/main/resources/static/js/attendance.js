c46f1d2b2cddaa63171dadd76c419f71b547ebd8
m-tsurumi <tsurumim@analix.co.jp>
2024-08-27 10:58:41 +0900

    fix:鶴見 各画面レイアウト修正

--- nippo_kintai_project/src/main/resources/static/js/attendance.js
+++ nippo_kintai_project/src/main/resources/static/js/attendance.js
@@ -1,80 +1,218 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const calendarBody = document.getElementById('calendar-body');
-   
+//申請承認後用勤務状況
+var statusList = {
+	0: "通常出勤",
+	1: "休日",
+	2: "祝日",
+	3: "遅刻",
+	4: "有給",
+	5: "欠勤",
+	6: "早退",
+	7: "時間外勤務",
+	8: "振替出勤",
+	9: "振替休日",
+	10: "代替出勤",
+	11: "代替休日",
 
-    let currentMonth = new Date().getMonth();
-    let currentYear = new Date().getFullYear();
+};
 
-    function generateCalendar(month, year) {
-        calendarBody.innerHTML = ''; // カレンダーをリセット
-        let daysInMonth = new Date(2024, 7 + 1, 0).getDate();
+function populateStatusDropdown() {
+	var dropdowns = document.getElementsByClassName('status-dropdown');
+	for (var i = 0; i < dropdowns.length; i++) {
+		var dropdown = dropdowns[i];
+		var selectedStatus = dropdown.getAttribute('data-status');
+		var options = '';
 
-        // カレンダーの行を生成
-        for (let date = 1; date <= daysInMonth; date++) {
-            let row = document.createElement('tr');
+		for (var key in statusList) {
+			var selected = (key == selectedStatus) ? ' selected' : '';
+			options += "<option value='" + key + "'" + selected + ">" + statusList[key] + "</option>";
+		}
 
-            // 日付セル
-            let dateCell = document.createElement('td');
-            dateCell.appendChild(document.createTextNode(`${year}-${month + 1}-${date}`));
-            row.appendChild(dateCell);
+		dropdown.innerHTML = options;
+	}
+}
 
-            // 曜日セル
-            let dayOfWeek = new Date(year, month, date).getDay();
-            let dayOfWeekCell = document.createElement('td');
-            dayOfWeekCell.appendChild(document.createTextNode(['日', '月', '火', '水', '木', '金', '土'][dayOfWeek]));
-            row.appendChild(dayOfWeekCell);
+window.onload = populateStatusDropdown;
 
-            // 勤務状況セル
-            let workStatusCell = document.createElement('td');
-            let workStatusInput = document.createElement('input');
-            workStatusInput.type = 'text';
-            workStatusCell.appendChild(workStatusInput);
-            row.appendChild(workStatusCell);
+// 申請情報確認時に使用
+document.addEventListener('DOMContentLoaded', function() {
+	// テーブル内のすべてのstatusセルを取得
+	var statusCells = document.querySelectorAll('td[id^="status-"]');
 
-            // 出勤時間セル
-            let startTimeCell = document.createElement('td');
-            let startTimeInput = document.createElement('input');
-            startTimeInput.type = 'time';
-            startTimeCell.appendChild(startTimeInput);
-            row.appendChild(startTimeCell);
-
-            // 退勤時間セル
-            let endTimeCell = document.createElement('td');
-            let endTimeInput = document.createElement('input');
-            endTimeInput.type = 'time';
-            endTimeCell.appendChild(endTimeInput);
-            row.appendChild(endTimeCell);
-
-            // 備考セル
-            let noteCell = document.createElement('td');
-            let noteInput = document.createElement('input');
-            noteInput.type = 'text';
-            noteCell.appendChild(noteInput);
-            row.appendChild(noteCell);
-
-            calendarBody.appendChild(row);
-        }
-    }
-
-    prevMonthButton.addEventListener('click', function () {
-        if (currentMonth === 0) {
-            currentMonth = 11;
-            currentYear--;
-        } else {
-            currentMonth--;
-        }
-        generateCalendar(currentMonth, currentYear);
-    });
-
-    nextMonthButton.addEventListener('click', function () {
-        if (currentMonth === 11) {
-            currentMonth = 0;
-            currentYear++;
-        } else {
-            currentMonth++;
-        }
-        generateCalendar(currentMonth, currentYear);
-    });
-
-    generateCalendar(currentMonth, currentYear);
+	// 各statusセルのテキストを変換
+	statusCells.forEach(function(cell) {
+		var statusCode = cell.textContent.trim();
+		var statusText = statusList[statusCode] || "不明";
+		cell.textContent = statusText;
+	});
 });
+
+
+//「却下」「承認」ボタンの活性化
+document.addEventListener('DOMContentLoaded', function() {
+	const urlParams = new URLSearchParams(window.location.search);
+	const activateButtons = urlParams.get('activateButtons');
+
+	if (activateButtons === 'true') {
+		document.getElementById('reject-btn').disabled = false;
+		document.getElementById('approve-btn').disabled = false;
+	}
+});
+
+
+// 入力チェック
+document.getElementById('display-form').addEventListener('submit', function(event) {
+	let year = document.getElementById('id_year').value;
+	let month = document.getElementById('id_month').value;
+	let validation = true;
+
+	if (year === "") {
+		document.getElementById('yearError').innerHTML = "年を入力してください";
+		validation = false;
+	} else if (year.length !== 4 || isNaN(year)) {
+		document.getElementById('yearError').innerHTML = "yyyyのフォーマットで入力してください";
+		validation = false;
+	} else {
+		document.getElementById('yearError').innerHTML = "";
+	}
+
+	if (month === "") {
+		document.getElementById('monthError').innerHTML = "月を入力してください";
+		validation = false;
+	} else if (isNaN(month) || month < 1 || month > 12) {
+		document.getElementById('monthError').innerHTML = "MMのフォーマットで入力してください";
+		validation = false;
+	} else {
+		document.getElementById('monthError').innerHTML = "";
+	}
+
+	if (!validation) {
+		event.preventDefault();
+	}
+});
+
+// 入力内容をローカルストレージに保存
+document.querySelector('#display-form').addEventListener('submit', function(event) {
+	var year = document.getElementById('id_year').value;
+	var month = document.getElementById('id_month').value;
+
+	// 月を2桁にフォーマット
+	if (month.length < 2) {
+		month = '0' + month;
+	}
+
+	var yearMonth = year + '-' + month;
+
+	var hiddenInput = document.getElementById('yearMonth');
+	hiddenInput.value = yearMonth;
+
+	sessionStorage.setItem('year', year);
+	sessionStorage.setItem('month', month);
+	sessionStorage.setItem('yearMonth', yearMonth);
+});
+
+// ページ読み込み時にローカルストレージから値を取得して設定
+window.addEventListener('load', function() {
+	var storedYear = sessionStorage.getItem('year');
+	var storedMonth = sessionStorage.getItem('month');
+	var storedYearMonth = localStorage.getItem('yearMonth');
+
+
+	if (storedYear) {
+		document.getElementById('id_year').value = storedYear;
+	}
+	if (storedMonth) {
+		// 月を2桁にフォーマットする
+		if (storedMonth.length < 2) {
+			storedMonth = '0' + storedMonth;
+		}
+		document.getElementById('id_month').value = storedMonth;
+	}
+	if (storedYearMonth) {
+		var hiddenInput = document.getElementById('yearMonth');
+		if (hiddenInput) {
+			hiddenInput.value = storedYearMonth;
+		}
+	}
+});
+
+//↓元々書いてあったやつ
+//document.addEventListener('DOMContentLoaded', function () {
+//    const calendarBody = document.getElementById('calendar-body');
+//
+//
+//    let currentMonth = new Date().getMonth();
+//    let currentYear = new Date().getFullYear();
+//
+//    function generateCalendar(month, year) {
+//        calendarBody.innerHTML = ''; // カレンダーをリセット
+//        let daysInMonth = new Date(2024, 7 + 1, 0).getDate();
+//
+//        // カレンダーの行を生成
+//        for (let date = 1; date <= daysInMonth; date++) {
+//            let row = document.createElement('tr');
+//
+//            // 日付セル
+//            let dateCell = document.createElement('td');
+//            dateCell.appendChild(document.createTextNode(`${year}-${month + 1}-${date}`));
+//            row.appendChild(dateCell);
+//
+//            // 曜日セル
+//            let dayOfWeek = new Date(year, month, date).getDay();
+//            let dayOfWeekCell = document.createElement('td');
+//            dayOfWeekCell.appendChild(document.createTextNode(['日', '月', '火', '水', '木', '金', '土'][dayOfWeek]));
+//            row.appendChild(dayOfWeekCell);
+//
+//            // 勤務状況セル
+//            let workStatusCell = document.createElement('td');
+//            let workStatusInput = document.createElement('input');
+//            workStatusInput.type = 'text';
+//            workStatusCell.appendChild(workStatusInput);
+//            row.appendChild(workStatusCell);
+//
+//            // 出勤時間セル
+//            let startTimeCell = document.createElement('td');
+//            let startTimeInput = document.createElement('input');
+//            startTimeInput.type = 'time';
+//            startTimeCell.appendChild(startTimeInput);
+//            row.appendChild(startTimeCell);
+//
+//            // 退勤時間セル
+//            let endTimeCell = document.createElement('td');
+//            let endTimeInput = document.createElement('input');
+//            endTimeInput.type = 'time';
+//            endTimeCell.appendChild(endTimeInput);
+//            row.appendChild(endTimeCell);
+//
+//            // 備考セル
+//            let noteCell = document.createElement('td');
+//            let noteInput = document.createElement('input');
+//            noteInput.type = 'text';
+//            noteCell.appendChild(noteInput);
+//            row.appendChild(noteCell);
+//
+//            calendarBody.appendChild(row);
+//        }
+//    }
+//
+//    prevMonthButton.addEventListener('click', function () {
+//        if (currentMonth === 0) {
+//            currentMonth = 11;
+//            currentYear--;
+//        } else {
+//            currentMonth--;
+//        }
+//        generateCalendar(currentMonth, currentYear);
+//    });
+//
+//    nextMonthButton.addEventListener('click', function () {
+//        if (currentMonth === 11) {
+//            currentMonth = 0;
+//            currentYear++;
+//        } else {
+//            currentMonth++;
+//        }
+//        generateCalendar(currentMonth, currentYear);
+//    });
+//
+//    generateCalendar(currentMonth, currentYear);
+//});
--- nippo_kintai_project/src/main/resources/static/js/userRegist.js
+++ nippo_kintai_project/src/main/resources/static/js/userRegist.js
@@ -1,78 +1,216 @@
-//バリデーションチェック
-document.getElementById('search-form').addEventListener('submit',function(event){
-  //バリデーションチェック
+//検索フォームのバリデーションチェック
+document.getElementById('search-form').addEventListener('submit', function(event) {
 
-// 変数の定義
-var halfwidthCheck =/^[0-9a-zA-Z\-\s]*$/
-let name = document.getElementById('search-name').value;
-let password = document.getElementById('password').value;
-let role = document.getElementById('role').value;
-let startDate = document.getElementById('startDate').value;
+	let name = document.getElementById('name').value;
+	const MAX_USERNAME_LENGTH = 20;
+	const regex = /^[^\\x00-\\x7F]*$/;
 
-const MAX_USERNAME_LENGTH = 20;
-const MAX_PASSWORD_LENGTH = 16;
-const MAX_STARTDATE_LENGTH = 10;
+	let validation = true;
 
-let validation = true;
+	if (name === "") {
+		document.getElementById('nameError').innerHTML = "名前を入力してください";
+		validation = false;
+	} else if (name.length > MAX_USERNAME_LENGTH) {
+		document.getElementById('nameError').innerHTML = "全角20文字以内で入力してください";
+		validation = false;
+	} else if (/^[0-9a-zA-Zｱ-ﾝ\-\s!-/:-@[-`{-~]*$/.test(name)) {
+		document.getElementById('nameError').innerHTML = "全角で入力してください";
+		validation = false;
+	} else {
+		document.getElementById('nameError').innerHTML = "";
+	}
+	if (!validation) {
+		event.preventDefault();
+		validation = false;
+	}
 
-  if(name === ""){
-    document.getElementById('nameError').innerHTML = "名前を入力して下さい";
-    validation = false;
-  } if(name.length >MAX_USERNAME_LENGTH){
-	  document.getElementById('nameError').innerHTML = "10文字以内で入力して下さい";
-    validation = false;
-    
-  }else{
-	  document.getElementById('nameError').innerHTML = "";
-  }
-  
-  
-   if(password === ""){
-    document.getElementById('password').innerHTML = "パスワードを入力して下さい";
-    validation = false;
-   } if(password.length >MAX_PASSWORD_LENGTH){
-	  document.getElementById('password').innerHTML = "16文字以内で入力して下さい";
-    validation = false;
-    
-    }if(!halfwidthCheck.test(password)){
-		document.getElementById('password').innerHTML = "半角で入力して下さい";
-	
-    
-  }else{
-	  document.getElementById('password').innerHTML = "";
-  }
-    
-    
-  
-  if(role === ""){
-    document.getElementById('role').innerHTML = "権限を選択して下さい";
-    validation = false;
-  } else{
-    document.getElementById('role').innerHTML = "";
-  }
-  
-  
-  if(startDate === ""){
-    document.getElementById('startDate').innerHTML = "利用開始日を入力して下さい";
-    validation = false;
- } if(startDate.length >MAX_STARTDATE_LENGTH){
-	  document.getElementById('startDate').innerHTML = "10文字以内で入力して下さい";
-    validation = false;
-    
-  }else{
-	  document.getElementById('startDate').innerHTML = "";
-  }
-  
-
-//  if(!check.test(email)){
-//    document.getElementById('message1').innerHTML = "メールアドレスの形式で入力して下さい";
-//    validation = false;
-//  }else{
-//    document.getElementById('message1').innerHTML = "";
-//  }
-
-  // バリデーションに引っかかった場合は送信させない
-  if(!validation){
-    event.preventDefault();
-  }
 });
+
+//検索フォーム内のユーザー名を登録フォームにコピー
+document.getElementById('regist-form').addEventListener('submit', function(event) {
+	const username = document.getElementById('name').value;
+	document.getElementById('hidden-name').value = username;
+	return true;
+});
+
+// 登録フォームのバリデーションチェック
+document.getElementById('regist-form').addEventListener('submit', function(event) {
+
+	let name = document.getElementById('name').value;
+	let password = document.getElementById('password').value;
+	let role = document.getElementById('role').value;
+	let departmentId = document.getElementById('departmentId').value;
+	let startDate = document.getElementById('startDate').value;
+	const commandDate = "9999/99/99";
+	let date = new Date(startDate);
+
+	const regex = /^[\\x00-\\x7F]*$/;
+
+	const MAX_USERNAME_LENGTH = 20;
+	const MAX_PASSWORD_LENGTH = 16;
+	const STARTDATE_LENGTH = 10;
+
+	let validation = true;
+
+	if (name === "") {
+		document.getElementById('nameError').innerHTML = "名前を入力してください";
+		validation = false;
+	} else if (name.length > MAX_USERNAME_LENGTH) {
+		document.getElementById('nameError').innerHTML = "全角20文字以内で入力してください";
+		validation = false;
+	} else if (/^[0-9a-zA-Zｱ-ﾝ\-\s!-/:-@[-`{-~]*$/.test(name)) {
+		document.getElementById('nameError').innerHTML = "全角で入力してください";
+		validation = false;
+	} else {
+		document.getElementById('nameError').innerHTML = "";
+	}
+
+	if (password === "") {
+		document.getElementById('passwordError').innerHTML = "パスワードを入力してください";
+		validation = false;
+	} else if (password.length > MAX_PASSWORD_LENGTH) {
+		document.getElementById('passwordError').innerHTML = "16文字以内で入力してください";
+		validation = false;
+	} else if (!/^[0-9a-zA-Z\-\s]*$/.test(password)) {
+		document.getElementById('passwordError').innerHTML = "半角で入力してください";
+		validation = false;
+	} else {
+		document.getElementById('passwordError').innerHTML = "";
+	}
+
+	if (role === "") {
+		document.getElementById('roleError').innerHTML = "権限を選択してください";
+		validation = false;
+	} else {
+		document.getElementById('roleError').innerHTML = "";
+	}
+	if (departmentId === "") {
+		document.getElementById('departmentIdError').innerHTML = "所属部署を選択してください";
+		validation = false;
+	} else {
+		document.getElementById('departmentIdError').innerHTML = "";
+	}
+
+	if (startDate === "") {
+		document.getElementById('startDateError').innerHTML = "利用開始日を入力してください";
+		validation = false;
+	} else if (startDate.length != STARTDATE_LENGTH) {
+		document.getElementById('startDateError').innerHTML = "10文字で入力してください";
+		validation = false;
+	} else if (startDate != commandDate && isNaN(date.getDate())) {
+		document.getElementById('startDateError').innerHTML = "利用開始日が不正です";
+		validation = false;
+	} else {
+		document.getElementById('startDateError').innerHTML = "";
+	}
+
+	if (!validation) {
+		event.preventDefault();
+
+	}
+
+
+});
+
+// パスワード表示・非表示	
+window.addEventListener('DOMContentLoaded', function() {
+	let btn_passview = document.getElementById("btn_passview");
+	let passwordInput = document.getElementById("password");
+	let viewIcon = document.getElementById("view");
+
+	passwordInput.type = 'password'; //初期状態はパスワードが隠れている状態
+	passwordInput.style.display = ''; // スタイルをリセットして表示する
+
+	btn_passview.addEventListener("click", function() {
+		if (passwordInput.type === 'password') {
+			passwordInput.type = 'text'; //表示
+			viewIcon.classList.remove('fa-eye-slash');
+			viewIcon.classList.add('fa-eye');
+
+		} else {
+			passwordInput.type = 'password'; //非表示
+			viewIcon.classList.remove('fa-eye');
+			viewIcon.classList.add('fa-eye-slash');
+		}
+	});
+});
+
+//↓元々書いてあったやつ
+////バリデーションチェック
+//document.getElementById('search-form').addEventListener('submit',function(event){
+//  //バリデーションチェック
+//
+//// 変数の定義
+//var halfwidthCheck =/^[0-9a-zA-Z\-\s]*$/
+//let name = document.getElementById('search-name').value;
+//let password = document.getElementById('password').value;
+//let role = document.getElementById('role').value;
+//let startDate = document.getElementById('startDate').value;
+//
+//const MAX_USERNAME_LENGTH = 20;
+//const MAX_PASSWORD_LENGTH = 16;
+//const MAX_STARTDATE_LENGTH = 10;
+//
+//let validation = true;
+//
+//  if(name === ""){
+//    document.getElementById('nameError').innerHTML = "名前を入力して下さい";
+//    validation = false;
+//  } if(name.length >MAX_USERNAME_LENGTH){
+//	  document.getElementById('nameError').innerHTML = "10文字以内で入力して下さい";
+//    validation = false;
+//    
+//  }else{
+//	  document.getElementById('nameError').innerHTML = "";
+//  }
+//  
+//  
+//   if(password === ""){
+//    document.getElementById('password').innerHTML = "パスワードを入力して下さい";
+//    validation = false;
+//   } if(password.length >MAX_PASSWORD_LENGTH){
+//	  document.getElementById('password').innerHTML = "16文字以内で入力して下さい";
+//    validation = false;
+//    
+//    }if(!halfwidthCheck.test(password)){
+//		document.getElementById('password').innerHTML = "半角で入力して下さい";
+//	
+//    
+//  }else{
+//	  document.getElementById('password').innerHTML = "";
+//  }
+//    
+//    
+//  
+//  if(role === ""){
+//    document.getElementById('role').innerHTML = "権限を選択して下さい";
+//    validation = false;
+//  } else{
+//    document.getElementById('role').innerHTML = "";
+//  }
+//  
+//  
+//  if(startDate === ""){
+//    document.getElementById('startDate').innerHTML = "利用開始日を入力して下さい";
+//    validation = false;
+// } if(startDate.length >MAX_STARTDATE_LENGTH){
+//	  document.getElementById('startDate').innerHTML = "10文字以内で入力して下さい";
+//    validation = false;
+//    
+//  }else{
+//	  document.getElementById('startDate').innerHTML = "";
+//  }
+//  
+//
+////  if(!check.test(email)){
+////    document.getElementById('message1').innerHTML = "メールアドレスの形式で入力して下さい";
+////    validation = false;
+////  }else{
+////    document.getElementById('message1').innerHTML = "";
+////  }
+//
+//  // バリデーションに引っかかった場合は送信させない
+//  if(!validation){
+//    event.preventDefault();
+//  }
+//});
