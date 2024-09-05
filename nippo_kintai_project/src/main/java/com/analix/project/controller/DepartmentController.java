3a5da35e2e2f450e521424f6c6c9b064a751b179
m-tsurumi <tsurumim@analix.co.jp>
2024-09-05 14:57:24 +0900

    fix:鶴見

--- nippo_kintai_project/src/main/java/com/analix/project/controller/DepartmentController.java
+++ nippo_kintai_project/src/main/java/com/analix/project/controller/DepartmentController.java
@@ -1,13 +1,44 @@
 package com.analix.project.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.analix.project.entity.Department;
+import com.analix.project.service.DepartmentService;
 
 @Controller
 public class DepartmentController {
+
+	@Autowired
+	private DepartmentService departmentService;
 	
+	/**
+	 * 初期表示
+	 * @param model
+	 * @return 部署登録画面
+	 */
 	@RequestMapping(path = "/department/regist")
-	public String departmentRegist() {
+	public String departmentRegist(Model model) {
+		
+		// 「登録済の部署」リストプルダウン
+		List<Department> departmentList = departmentService.showDepartment();
+		model.addAttribute("departmentList", departmentList);
+
 		return "/department/regist";
+	}
+	
+	/**
+	 * 
+	 */
+	@RequestMapping(path = "/department/regist/complete", method = RequestMethod.POST)
+	public String departmentComplete() {
+		
+		return "/department/regist";
+		
 	}
 }
---
+++ nippo_kintai_project/src/main/java/com/analix/project/service/DepartmentService.java
@@ -0,0 +1,48 @@
+package com.analix.project.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.analix.project.entity.Department;
+import com.analix.project.mapper.DepartmentMapper;
+
+@Service
+public class DepartmentService {
+	
+	@Autowired
+	DepartmentMapper departmentMapper;
+	
+	/**
+	 * 「登録済の部署」リスト
+	 * @return 部署リスト
+	 */
+	public List<Department> showDepartment(){
+		List<Department> departmentList = departmentMapper.findAllDepartmentName();
+		return departmentList;
+	}
+	
+	/**
+	 * 部署の存在チェック
+	 * @param name
+	 * @return
+	 */
+	public boolean isDepartmentExists(String name){
+		Integer departmentCount = departmentMapper.departmentCountByName(name);
+		return departmentCount > 0;
+	}
+	
+	/**
+	 * 「登録」ボタン押下
+	 */
+	public String registDepartment(Department department) {
+		if (isDepartmentExists(department.getName())) {
+			return "この部署名は既に登録済です";
+		}
+		departmentMapper.registDepartment(department);
+		return "登録しました";
+	}
+
+
+}
