1a274652f8b83cedabe4ce3cc989bdd7d2a86613
m-tsurumi <tsurumim@analix.co.jp>
2024-09-10 09:39:51 +0900

    fix:鶴見 入力チェック(文字数、ボタンの活性非活性効かなかった用)

--- nippo_kintai_project/src/main/java/com/analix/project/entity/Department.java
+++ nippo_kintai_project/src/main/java/com/analix/project/entity/Department.java
@@ -5,6 +5,10 @@ public class Department {
 	private Integer departmentId;
 	private String name;
 	private Byte isActive;
+	private String newName;
+	private String exsistsName;
+	private String inactiveName;
+
 
 	public Integer getDepartmentId() {
 		return departmentId;
@@ -29,4 +33,30 @@ public class Department {
 	public void setIsActive(Byte isActive) {
 		this.isActive = isActive;
 	}
+
+	public String getNewName() {
+		return newName;
+	}
+
+	public void setNewName(String newName) {
+		this.newName = newName;
+	}
+
+	public String getExsistsName() {
+		return exsistsName;
+	}
+
+	public void setExsistsName(String exsistsName) {
+		this.exsistsName = exsistsName;
+	}
+
+	public String getInactiveName() {
+		return inactiveName;
+	}
+
+	public void setInactiveName(String inactiveName) {
+		this.inactiveName = inactiveName;
+	}
+	
+	
 }
