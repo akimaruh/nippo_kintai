8261d6cdc90e72985f4b1bbd884c2297bcf3db33
a-akimaru <akimaruh@analix.co.jp>
2024-09-17 14:08:14 +0900

    fix:秋丸　定数クラス更新

--- nippo_kintai_project/src/main/java/com/analix/project/util/Constants.java
+++ nippo_kintai_project/src/main/java/com/analix/project/util/Constants.java
@@ -3,6 +3,18 @@ package com.analix.project.util;
  * 定数クラス
  */
 public interface Constants {
+	/** 改行指定 **/
+	public static final String LINE_SEPARATOR = System.getProperty("line.separator");
+	
+	/** ユーザー権限:社員 **/
+	public static final String CODE_VAL_ADMIN ="Admin";
+	/** ユーザー権限:マネージャ **/
+	public static final String CODE_VAL_MANAGER ="Manager";
+	/** ユーザー権限:ユニットマネージャ **/
+	public static final String CODE_VAL_UNITMANAGER ="UnitManager";
+	/** ユーザー権限:社員 **/
+	public static final String CODE_VAL_REGULAR ="Regular";
+	
 	/** 日報提出ステータス：未提出 */
 	public static final Integer CODE_VAL_UNSUBMITTED = 0;
 	/** 日報提出ステータス：提出済承認前 */
