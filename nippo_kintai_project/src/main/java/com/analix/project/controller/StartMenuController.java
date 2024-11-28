188bd02331336486b0660350d7c1c38fb9db6d6b
m-tsurumi <tsurumim@analix.co.jp>
2024-11-28 16:18:00 +0900

    fix:鶴見　プッシュ漏れ分

--- nippo_kintai_project/src/main/java/com/analix/project/controller/StartMenuController.java
+++ nippo_kintai_project/src/main/java/com/analix/project/controller/StartMenuController.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -77,6 +78,12 @@ public class StartMenuController {
 		redirectAttributes.addFlashAttribute("message", "ログオフしました。再度ログインしてください。");
 
 		return "redirect:/";
+	}
+	
+	// パスワード再設定確認用　あとでけす
+	@GetMapping("/password/regist")
+	public String passwordRegist() {
+		return "/password/regist";
 	}
 
 }
