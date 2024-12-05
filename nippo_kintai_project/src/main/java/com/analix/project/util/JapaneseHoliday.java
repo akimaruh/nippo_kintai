444d821d5e152c3f51770ab623a083a024eee84f
m-tsurumi <tsurumim@analix.co.jp>
2024-12-05 14:22:59 +0900

    fix:鶴見 勤怠管理画面(社員)の勤怠表の今日の日付に色を付ける、土日の文字に色を付ける処理を追加

---
+++ nippo_kintai_project/src/main/java/com/analix/project/util/JapaneseHoliday.java
@@ -0,0 +1,13 @@
+package com.analix.project.util;
+
+public class JapaneseHoliday {
+	
+//	public static void getHoliday() {
+//	
+//	BusinessCalendar.newBuilder().holiday(Japan.PUBLIC_HOLIDAYS).build()
+//    .getHolidaysBetween(LocalDate.of(2023, 1, 1), LocalDate.of(2023, 12, 31))
+//    .forEach(System.out::println);
+//	
+//	}
+
+}
